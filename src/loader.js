ed9b8340f341f9515f4f1e6a9f3ad871b089c856
MasuqaT <occar@hotmail.co.jp>
2018-03-04 19:04:12 +0900

    Refactor test file.

--- src/loader.js
+++ src/loader.js
@@ -4,7 +4,37 @@ import validateOptions from 'schema-utils';
 
 const schema = {
     type: 'object',
-    properties: {}
+    properties: {
+        transmitRules: {
+            type: 'array',
+            minItems: 1,
+            items: {
+                type: 'object',
+                properties: {
+                    test: {
+                        type: 'object' // RegExp
+                    },
+                    targets: {
+                        type: 'array',
+                        minItems: 1,
+                        items: {
+                            type: 'string'
+                        }
+                    }
+                },
+                required: ['test', 'targets']
+            }
+        },
+        noCreate: {
+            type: 'boolean'
+        }
+    },
+    required: ['transmitRules']
+};
+
+const getObjectType = (obj) => {
+    const str = Object.prototype.toString.call(obj); // [object FooBar]
+    return str.substring(8, str.length - 1);
 };
 
 // noinspection JSUnusedGlobalSymbols
@@ -16,12 +46,19 @@ const schema = {
 export default function loader(source) {
     const options = getOptions(this) || {};
 
-    validateOptions(schema, options, 'Transmit Update Loader');
+    //region Validation
+    validateOptions(schema, options, 'Options for transmit-update-loader are invalid.');
+
+    if (options.transmitRules.some(r => getObjectType(r.test) !== 'RegExp')) {
+        throw new Error('`transmitRules.test` should be regex in transmit-update-loader.');
+    }
+    //endregion Validation
 
     const callback = this.async();
 
     touch(this.resourcePath, {
-        mtime: true
+        mtime: true,
+        nocreate: options.nocreate === true
     }, () => {
         callback(null, source);
     });
--- test/loader.test.js
+++ test/loader.test.js
@@ -1,23 +1,35 @@
+/* eslint-env jest */
+
 import compiler from './compiler.js';
 import fs from 'fs';
 import path from 'path';
 import {promisify} from 'util';
 
 /**
- * @var {jest.Expect} expect
- */
-
-/**
- * @param fileName
+ * @param {string} fileName
  * @returns {Promise<string>}
  */
 const readFileAsync = (fileName) => promisify(fs.readFile)(path.join(__dirname, fileName)).then(b => b.toString());
 
 /**
- * @param fileName
+ * @param {string} fileName
  * @returns {Promise<"fs".Stats>}
  */
 const fileStatAsync = (fileName) => promisify(fs.stat)(path.join(__dirname, fileName));
+
+/**
+ * @param {string} dirPath
+ * @returns {Promise<array<string>>}
+ */
+const enumerateFilesAsync = (dirPath) => promisify(fs.readdir)(dirPath)
+    .then(files => files.map(f => path.join(dirPath, f)).filter(f => fs.statSync(f).isFile()));
+
+afterEach(async () => {
+    const files = await enumerateFilesAsync(path.join(__dirname, 'fixtures'));
+    files.filter(f => f.endsWith('.null')).forEach(f => {
+        fs.unlinkSync(f);
+    });
+});
 
 test('does not affect file content', async () => {
     const targetFileName = './fixtures/foo.txt';
@@ -26,10 +38,23 @@ test('does not affect file content', async () => {
     expect(output).toBe(await readFileAsync(targetFileName));
 });
 
-test('renews ctime of the file', async () => {
+test('rejects empty option', async () => {
     const targetFileName = './fixtures/foo.txt';
-    const preStat = await fileStatAsync(targetFileName);
-    await compiler(targetFileName, {});
-    const postStat = await fileStatAsync(targetFileName);
-    expect(postStat.ctimeMs).toBeGreaterThan(preStat.ctimeMs);
+    expect(async () => {
+        await compiler(targetFileName, {});
+    }).toThrowError(/transmitRules\.test/);
 });
+
+// TODO transform function is success?
+
+// test('does not create a new file by default', async () => {
+//
+// });
+
+// test('renews ctime of the file', async () => {
+//     const targetFileName = './fixtures/foo.txt';
+//     const preStat = await fileStatAsync(targetFileName);
+//     await compiler(targetFileName, {});
+//     const postStat = await fileStatAsync(targetFileName);
+//     expect(postStat.ctimeMs).toBeGreaterThan(preStat.ctimeMs);
+// });
